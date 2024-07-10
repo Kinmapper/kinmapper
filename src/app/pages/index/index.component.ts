e75f694a102affb313868aeb7a934e4fe994d542
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2024-07-10 01:52:09 +0000

    chore: Added @theinternetfolks/snowflake library feat: Added a floating action button containing a button to create a kinmap

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Snowflake } from "@theinternetfolks/snowflake";
+import { MenuItem } from 'primeng/api';
+import { SpeedDialModule } from 'primeng/speeddial';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [],
+  imports: [
+    SpeedDialModule
+  ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
 export class IndexComponent {
+  fabMenuItems!: MenuItem[];
 
+  constructor(private _router: Router) { }
+
+  ngOnInit() {
+    this.fabMenuItems = [
+      {
+        icon: 'pi pi-file-plus',
+        routerLink: [`/kinmap/${Snowflake.generate()}`]
+      }
+    ];
+  }
+
+  public viewUpdateKinmap(UUID: string): void {
+    this._router.navigate([`/kinmap/${UUID}`]);
+  }
 }
