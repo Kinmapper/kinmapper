c01b0d30c973c4ae6a7ca10fc6040185b3caad97
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2024-07-10 01:03:37 +0000

    feat: Minor commit

--- src/app/pages/kinmap/kinmap.component.ts
+++ src/app/pages/kinmap/kinmap.component.ts
@@ -31,51 +31,95 @@ import { ToolbarModule } from 'primeng/toolbar';
 export class KinmapComponent {
   displayIndividualAdditionDialog: boolean = false;
   displayAssociationAdditionDialog: boolean = false;
+
+  kinmapTitle: String = '';
   options!: EChartsOption;
 
   constructor() { }
 
   ngOnInit(): void {
-    const xAxisData = [];
-    const data1 = [];
-    const data2 = [];
-
-    for (let i = 0; i < 100; i++) {
-      xAxisData.push('category' + i);
-      data1.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
-      data2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
-    }
-
     this.options = {
-      legend: {
-        data: ['bar', 'bar2'],
-        align: 'left',
+      title: {
+        text: 'Simple Graph',
       },
       tooltip: {},
-      xAxis: {
-        data: xAxisData,
-        silent: false,
-        splitLine: {
-          show: false,
-        },
-      },
-      yAxis: {},
+      animationDurationUpdate: 1500,
+      animationEasingUpdate: 'quinticInOut',
       series: [
         {
-          name: 'bar',
-          type: 'bar',
-          data: data1,
-          animationDelay: idx => idx * 10,
-        },
-        {
-          name: 'bar2',
-          type: 'bar',
-          data: data2,
-          animationDelay: idx => idx * 10 + 100,
+          type: 'graph',
+          layout: 'force', // Change layout to 'force'
+          symbolSize: 60,
+          roam: true,
+          label: {
+            show: true,
+          },
+          edgeSymbol: ['circle', 'arrow'],
+          edgeSymbolSize: [4, 10],
+          edgeLabel: {
+            fontSize: 20,
+          },
+          data: [
+            { name: 'Node 1' },
+            { name: 'Node 2' },
+            { name: 'Node 3' },
+            { name: 'Node 4' },
+            { name: 'Node 5' }
+          ],
+          links: [
+            {
+              source: 0,
+              target: 1,
+              symbolSize: [5, 20],
+              label: {
+                show: true,
+              },
+              lineStyle: {
+                width: 5,
+                curveness: 0.2,
+              },
+            },
+            {
+              source: 'Node 2',
+              target: 'Node 1',
+              label: {
+                show: true,
+              },
+              lineStyle: {
+                curveness: 0.2,
+              },
+            },
+            {
+              source: 'Node 1',
+              target: 'Node 3',
+            },
+            {
+              source: 'Node 2',
+              target: 'Node 3',
+            },
+            {
+              source: 'Node 2',
+              target: 'Node 4',
+            },
+            {
+              source: 'Node 1',
+              target: 'Node 4',
+            },
+            {
+              source: 'Node 1',
+              target: 'Node 5',
+            }
+          ],
+          lineStyle: {
+            opacity: 0.9,
+            width: 2,
+            curveness: 0,
+          },
+          force: {
+            repulsion: 1000, // Adjust this value to control the distance between nodes
+          },
         },
       ],
-      animationEasing: 'elasticOut',
-      animationDelayUpdate: idx => idx * 5,
     };
   }
 }
